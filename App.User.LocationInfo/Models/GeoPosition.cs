6d46844fd6f84ee62091cffe12b252b80abbf244
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-30 13:48:05 +0000

    GeoPosition class added.

---
+++ App.User.LocationInfo/Models/GeoPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.User.LocationInfo.Models
+{
+    public class GeoPosition
+    {
+        public double Latitude { get; }
+        public double Longitude { get; }
+
+        public GeoPosition(double Lat, double Lng)
+        {
+            Latitude = Lat;
+            Longitude = Lng;
+        }
+    }
+}
