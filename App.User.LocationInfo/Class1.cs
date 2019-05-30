f28f81ace97ed85f1ee2945e3b2559d04a3d1652
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-30 15:42:20 +0000

    removes unsed class and added TrackingService.

--- App.User.LocationInfo/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace App.User.LocationInfo
-{
-    public class Class1
-    {
-    }
-}
---
+++ App.User.LocationInfo/Services/TrackingService.cs
@@ -0,0 +1,23 @@
+﻿/*
+TrackingService.cs
+30/05/2019 14:53:23
+Kodjo Laurent Egbakou
+*/
+
+using App.User.LocationInfo.Constants;
+using App.User.LocationInfo.Models;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace App.User.LocationInfo.Services
+{
+    /// <summary>
+    /// Defines the <see cref="TrackingService" />
+    /// </summary>
+    public class TrackingService
+    {
+        
+    }
+}
