aae31f9a95e2bd169add49fdd67fa4739c7df9f2
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-30 13:45:59 +0000

    API resources added.

---
+++ App.User.LocationInfo/Constants/APIResources.cs
@@ -0,0 +1,9 @@
+﻿namespace App.User.LocationInfo.Constants
+{
+    public class APIResources
+    {
+        public static string IpifyApi = "https://api.ipify.org/?format=json";
+        public static string IpApi = "https://ipapi.co/json";
+        public static string RestcountriesAp = "https://restcountries.eu/rest/v2/name/";
+    }
+}
